10c153686af5852413e2ad8341ee8171880225a5
Ezeokeke Collins Chidubem <90806850+CollinsEzeokeke@users.noreply.github.com>
2025-02-12 10:41:07 -0800

    Add NavBar component to Gallery page for improved navigation

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -1,8 +1,10 @@
 import Index from "@/components/galleryComp";
+import { NavBar } from "@/components/nav-bar";
 
 export default function Gallery() {
   return (
     <>
+      <NavBar />
       <Index />
     </>
   );
